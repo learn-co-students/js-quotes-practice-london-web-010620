e49680b9d10ee7d5b4aaa1feefd1077e85068f21
Wojciech Bojarski <bojawojt@gmail.com>
2020-02-27 13:43:03 +0000

    v1 new post, delete and likes

--- src/index.js
+++ src/index.js
@@ -1,112 +1,107 @@
+const API_URL = "http://localhost:3000";
+const QUOTES_URL = `${API_URL}/quotes`;
+const LIKES_URL = `${API_URL}/likes`;
+const QUOTES_WITH_LIKES_URL = `${API_URL}/quotes?_embed=likes`;
+const quotesList = document.getElementById("quote-list");
 
+//render each quote
+const renderQuote = quote => {
+  const newQuote = document.createElement("li");
+  newQuote.className = "quote-card";
 
-const quoteList = document.querySelector("#quote-list")
-const newQuoteForm = document.querySelector("#new-quote-form")
+  const blockQuote = document.createElement("blockquote");
+  blockQuote.className = "blockquote";
 
-newQuoteForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-  // console.log(newQuoteForm.quote)
-  // create a new quote and add to our list
-  // get the user input 
-  const newQuote = document.querySelector("#new-quote").value
-  const newAuthor = document.querySelector("#author").value
+  const quoteContent = document.createElement("p");
+  quoteContent.innerText = quote.quote;
+  blockQuote.append(quoteContent);
 
-  // once we get the user input, make fetch happen
-  // POST /quotes
-  fetch("http://localhost:3000/quotes", {
+  const quoteFooter = document.createElement("footer");
+  quoteFooter.className = "blockquote-footer";
+  quoteFooter.innerText = quote.author;
+  blockQuote.append(quoteFooter);
+
+  const likeSpan = document.createElement("span");
+  likeSpan.innerText = quote.likes.length;
+
+  const likeButton = document.createElement("button");
+  likeButton.innerText = "Likes: ";
+  likeButton.className = "btn-success";
+  likeButton.addEventListener("click", () => addLikes(quote, likeSpan));
+
+  likeButton.append(likeSpan);
+  blockQuote.append(likeButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "btn-danger";
+  deleteButton.addEventListener("click", () => deleteQuote(quote, newQuote));
+  blockQuote.append(deleteButton);
+
+  newQuote.append(blockQuote);
+  quotesList.append(newQuote);
+};
+//iterate through each quote calling render on each of them
+const renderQuotes = quotes => {
+  quotes.forEach(quote => renderQuote(quote));
+};
+
+//add quote
+const formEl = document.getElementById("new-quote-form");
+
+const postQuote = quote => {
+  return fetch(QUOTES_URL, {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      quote: newQuote,
-      author: newAuthor
-    })
+    body: JSON.stringify(quote)
+  }).then(res => res.json());
+};
+
+formEl.addEventListener("submit", event => {
+  event.preventDefault();
+  postQuote({
+    quote: event.target.elements.quote.value,
+    author: event.target.elements.author.value,
+    likes: []
   })
-    .then(r => r.json())
-    .then(newQuoteObj => {
-      renderSingleQuote(newQuoteObj)
-    })
+    .then(renderQuote)
+    .then(() => event.target.reset());
+});
 
+//delete quote
+const deleteQuote = (quote, quoteItem) => {
+  fetch(`${QUOTES_URL}/${quote.id}`, {
+    method: "DELETE"
+  }).then(() => {
+    quoteItem.remove();
+  });
+};
 
-  // then add it to our list of quotes
+//add likes
 
-})
-
-function renderSingleQuote(quote) {
-  const newLi = document.createElement("li")
-  newLi.className = "quote-card"
-
-  let likesCount;
-  if (quote.likes) {
-    likesCount = quote.likes.length
-  } else {
-    likesCount = 0
-  }
-
-  newLi.innerHTML = `
-  <blockquote id=${quote.id} class="blockquote">
-    <p class="mb-0">${quote.quote}</p>
-    <footer class="blockquote-footer">${quote.author}</footer>
-    <br>
-    <button class='btn-success'>Likes: <span>${likesCount}</span></button>
-    <button class='btn-danger'>Delete</button>
-  </blockquote>
-  `
-
-  // NESTED EVENT LISTENERS
-  const deleteBtn = newLi.querySelector(".btn-danger")
-  const likeBtn = newLi.querySelector(".btn-success")
-
-  likeBtn.addEventListener("click", () => {
-    // debugger
-    fetch("http://localhost:3000/likes", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        quoteId: quote.id
-      })
-    })
-      .then(r => r.json())
-      .then(() => {
-        const likesSpan = newLi.querySelector("span")
-        likesSpan.textContent = parseInt(likesSpan.textContent) + 1
-      })
+const addLikes = (quote, span) => {
+  fetch(`${LIKES_URL}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({ quoteId: quote.id })
   })
+    .then(res => res.json())
+    .then(data => {
+      span.innerText = parseInt(span.innerText) + 1;
+    });
+};
 
-  deleteBtn.addEventListener("click", (event) => {
-    // remove the quote from our list in the DOM
-    newLi.remove()
+//fetch quotes with likes
+const getQuotesData = () => {
+  fetch(QUOTES_WITH_LIKES_URL)
+    .then(resp => resp.json())
+    .then(quotes => renderQuotes(quotes));
+};
 
-    // do a fetch to delete on the server
-    // DELETE /quotes/:id
-    fetch(`http://localhost:3000/quotes/${quote.id}`, {
-      method: "DELETE"
-    })
-  })
-
-  quoteList.append(newLi)
-}
-
-// function handleDelete(event) {
-//   event.target.closest(".quote-card").remove()
-// }
-
-// function to parse thru the data
-function renderAllQuotes(quoteArray) {
-  // and render each element to the page
-  quoteArray.forEach(quote => {
-    renderSingleQuote(quote)
-  })
-}
-
-// Submitting the form creates a new quote 
-// and adds it to the list of quotes without 
-// having to refresh the page. Pessimistic rendering is reccommended.
-
-fetch("http://localhost:3000/quotes?_embed=likes")
-  .then(r => r.json())
-  .then(quoteArray => renderAllQuotes(quoteArray))
+document.addEventListener("click", getQuotesData());
